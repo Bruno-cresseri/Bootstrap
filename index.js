15715c862b8686ee12eae8fc48e413736adb3ed4
Bruno Cresseri <brunocresseri.1@gmail.com>
2023-10-08 20:38:50 -0300

    push

---
+++ index.js
@@ -0,0 +1,15 @@
+// document.addEventListener('DOMContentLoaded', function () {
+// const boton = document.getElementById("place");
+// const menu = document.getElementById("menuDesplegable");
+
+// boton.addEventListener("click", function() {
+//   // Cambia la visibilidad del menú desplegable al hacer clic en el botón
+//   if (menu.style.display === "block") {
+//     menu.style.display = "none";
+//   } else {
+//     menu.style.display = "block";
+//   }
+// });
+
+
+// });
