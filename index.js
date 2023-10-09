2909e71b5c36c8946b5a366fe43e4482aef9a1ad
Bruno Cresseri <brunocresseri.1@gmail.com>
2023-10-09 00:25:19 -0300

    push último

--- index.js
+++ index.js
@@ -1,15 +1,36 @@
-// document.addEventListener('DOMContentLoaded', function () {
-// const boton = document.getElementById("place");
-// const menu = document.getElementById("menuDesplegable");
+document.addEventListener('DOMContentLoaded', function () {
+// Obtén una referencia al botón por su ID
+const placeButton = document.getElementById("place");
+const contenidoDesplegable = document.getElementById("contenidoDesplegable");
 
-// boton.addEventListener("click", function() {
-//   // Cambia la visibilidad del menú desplegable al hacer clic en el botón
-//   if (menu.style.display === "block") {
-//     menu.style.display = "none";
-//   } else {
-//     menu.style.display = "block";
-//   }
-// });
+placeButton.addEventListener("click", function() {
+  // Verifica si el atributo aria-expanded es "false"
+  if (placeButton.getAttribute("aria-expanded") === "false") {
+    // Cambia el valor del atributo aria-expanded a "true"
+    placeButton.setAttribute("aria-expanded", "true");
+    // Muestra el contenido desplegable
+    contenidoDesplegable.style.display = "block";
+  } else {
+    // Cambia el valor del atributo aria-expanded a "false"
+    placeButton.setAttribute("aria-expanded", "false");
+    // Oculta el contenido desplegable
+    contenidoDesplegable.style.display = "none";
+  }
+});
+    
+    
+    
+    
+    
+    document.getElementById("opciones").addEventListener("click", function () {
+    const opcionesDesplegadas = document.getElementById("opcionesDesplegadas");
+    if (opcionesDesplegadas.style.display === "block") {
+        opcionesDesplegadas.style.display = "none";
+    } else {
+        opcionesDesplegadas.style.display = "block";
+    }
+});
 
 
-// });
+
+});
